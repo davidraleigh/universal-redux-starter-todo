efff70a33d91a44f4f08b0a5a33c78d7a620a923
David Raleigh <davidraleigh@gmail.com>
2016-02-04 17:19:48 -0800

    going through tutorial and updating

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,19 @@
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const COMPLETE_TODO = 'COMPLETE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+export const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+// TODO the nextTodoId should be a unique identifier instead of an incremented integer
 let nextTodoId = 0
 export const addTodo = (text) => {
   return {
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     id: nextTodoId++,
     text
   }
@@ -9,14 +21,14 @@ export const addTodo = (text) => {
 
 export const setVisibilityFilter = (filter) => {
   return {
-    type: 'SET_VISIBILITY_FILTER',
+    type: SET_VISIBILITY_FILTER,
     filter
   }
 }
 
 export const toggleTodo = (id) => {
   return {
-    type: 'TOGGLE_TODO',
+    type: TOGGLE_TODO,
     id
   }
 }
